c1613e613da7297d8e3cc7a9f64921f55a397563
Marcelo Inacio <marcelomarkg@gmail.com>
2021-06-02 04:05:20 -0300

    commit2

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Conversor from "./components/Conversor"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Conversor de moedas</h1>
+        <div className="linha">
+       <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+       <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+      </div>
+      <div className="linha">
+       <Conversor moedaA="CAD" moedaB="BRL"></Conversor>
+       <Conversor moedaA="BRL" moedaB="CAD"></Conversor>
+      </div>
+      </div>
+      );
+    }
+  }
+
+
+export default App;
